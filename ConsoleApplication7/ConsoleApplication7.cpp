982a92c58817036bedb5fdea686a6983a3865152
youtube-programmercpp <sbl908k@yahoo.co.jp>
2022-10-15 13:57:38 +0900

    UI Automation サンプルを追加

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,73 @@
+﻿//UI Automation 利用サンプル C++ コード Copyright(C) 2022 https://www.youtube.com/@ProgrammerCpp
+#include <Windows.h>
+#include <UIAutomation.h>
+
+// --- COM 応用記述 ---
+#include <comdef.h>
+#define	Macro(name)	_COM_SMARTPTR_TYPEDEF(name, __uuidof(name))
+Macro(IUIAutomation         );
+Macro(IUIAutomationCondition);
+Macro(IUIAutomationElement  );
+#undef	Macro
+// COM の関数呼び出しが成功したことを確認する
+void VerifyComError(HRESULT hr)
+{
+	if (FAILED(hr))
+		_com_issue_error(hr);
+}
+// --------------------
+
+// --- UI Automation 応用サブルーチン ---
+ // AutomationId 一致条件を生成
+IUIAutomationConditionPtr CreateAutomationIdPropertyCondition(const IUIAutomationPtr& pUIAutomation, const _variant_t& AutomationId)
+{
+	IUIAutomationConditionPtr pCondition;
+	VerifyComError(pUIAutomation->CreatePropertyCondition(UIA_AutomationIdPropertyId, AutomationId, &pCondition));
+	return pCondition;
+}
+// 条件に合う UI 要素を検索
+IUIAutomationElementPtr FindElement(const IUIAutomationPtr& pUIAutomation, const IUIAutomationConditionPtr& pCondition)
+{
+	IUIAutomationElementPtr pRootElement;
+	VerifyComError(pUIAutomation->GetRootElement(&pRootElement));
+
+	IUIAutomationElementPtr found;
+	VerifyComError(pRootElement->FindFirst(TreeScope_Descendants, pCondition, &found));
+	return found;
+}
+// 条件に合う UI 要素の HWND を取得
+HWND NativeWindowHandleByCondition(const IUIAutomationPtr& pUIAutomation, const IUIAutomationConditionPtr& pCondition)
+{
+	if (const auto pUIElement = FindElement(pUIAutomation, pCondition)) {
+		UIA_HWND hWnd;
+		VerifyComError(pUIElement->get_CurrentNativeWindowHandle(&hWnd));
+		return HWND(hWnd);
+	}
+	else
+		return nullptr;//該当なし
+}
+// --------------------------------------
+int main()
+{
+	// CoInitializeEx(COINIT_MULTITHREADED) は UI Automation の使い初めに必要
+	if (SUCCEEDED(CoInitializeEx(nullptr, COINIT_MULTITHREADED))) {
+		try {
+			// UIAutomation オブジェクトを用意
+			IUIAutomationPtr pUIAutomation;
+			VerifyComError(CoCreateInstance(CLSID_CUIAutomation, nullptr, CLSCTX_INPROC_SERVER, IID_IUIAutomation, (void**)&pUIAutomation));
+			// AutomationId が "textBox1" である UI 要素の HWND を取得し、そのテキスト内容を変更する
+			if (const auto hWnd = NativeWindowHandleByCondition(pUIAutomation, CreateAutomationIdPropertyCondition(pUIAutomation, L"textBox1")))
+				SendMessageA(hWnd, WM_SETTEXT, 0, LPARAM("C++プログラムから送信された文字列"));
+		}
+		catch (const _com_error& e) {
+			OutputDebugString(e.ErrorMessage());
+			OutputDebugString(TEXT("\n"));
+		}
+		// UI Automation を使い終わったら CoUninitialize を呼ぶ。（使い終わっていないのに呼んだら駄目）
+		CoUninitialize();
+	}
+}
+/*
+* UI Automation 基本参考資料
+* https://learn.microsoft.com/en-us/windows/win32/winauto/entry-uiauto-win32
+*/
